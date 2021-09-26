f348b5624f7593a5f7ab942b740a18b289ce6b9d
Ahmet Safa Bülbül <ahmetsafabulbul@gmail.com>
2021-09-26 18:31:30 +0300

    now post pages are created by gatsby-node

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -5,32 +5,31 @@ import { GatsbyImage } from "gatsby-plugin-image";
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
 }) {
-  const { markdownRemark } = data; // data.markdownRemark holds your post data
-  const { frontmatter, html } = markdownRemark;
+  const { markdownRemark: post } = data; // data.markdownRemark holds your post data
   return (
     <>
       <div className="h-1/4 w-screen flex">
         <GatsbyImage
           className="rounded-sm object-cover w-full"
-          alt={frontmatter.title}
+          alt={post.frontmatter.title}
           image={
-            frontmatter.featureImage
+            post.frontmatter.featureImage
               .childImageSharp.gatsbyImageData
           }
         />
       </div>
       <div className="flex flex-col mt-4 px-4 gap-y-4">
         <span className="font-body text-gray-500 text-left">
-          {frontmatter.date}
+          {post.frontmatter.date}
         </span>
         <h1 className="font-display text-xl text-center">
-          {frontmatter.title}
+          {post.frontmatter.title}
         </h1>
 
         <div
           className="text-black font-body font-medium text-justify"
           dangerouslySetInnerHTML={{
-            __html: html,
+            __html: post.html,
           }}
         />
       </div>
@@ -39,8 +38,10 @@ export default function Template({
 }
 
 export const pageQuery = graphql`
-  query ($id: String!) {
-    markdownRemark(id: { eq: $id }) {
+  query BlogPostByPath($path: String!) {
+    markdownRemark(
+      frontmatter: { slug: { eq: $path } }
+    ) {
       html
       frontmatter {
         date(formatString: "MMMM DD, YYYY")
