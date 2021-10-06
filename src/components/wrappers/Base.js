429e4e8d49510aeb2170c72fa98a8bf2c6a1ac46
Ahmet Safa Bülbül <ahmetsafabulbul@gmail.com>
2021-10-07 02:15:26 +0300

    wrappers deleted

--- src/components/wrappers/Base.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const BaseWrapper = ({ children }) => (
-  <div className="px-mobile laptop:px-wrapper">
-    {children}
-  </div>
-);
-
-export default BaseWrapper;
--- src/components/wrappers/index.js
+++
@@ -1,1 +0,0 @@
-export { default as WrapperBase } from "./Base";
