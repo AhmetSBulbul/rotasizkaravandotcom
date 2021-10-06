9b7c6ce766dc38a5115f23bfec947297e6ce972f
Ahmet Safa Bülbül <ahmetsafabulbul@gmail.com>
2021-10-06 21:01:48 +0300

    now worklink use card components

--- src/components/WorkLink/index.js
+++ src/components/WorkLink/index.js
@@ -1,35 +1,26 @@
 import React from "react";
 
 import * as styles from "./index.module.css";
-import { GatsbyImage } from "gatsby-plugin-image";
-import { Link } from "gatsby";
 
-const WorkLink = ({ post }) => (
-  <div className={styles.container}>
-    <GatsbyImage
-      className={styles.featureImage}
-      key={post.id}
-      objectFit="cover"
-      image={
-        post.frontmatter.featureImage
-          .childImageSharp.gatsbyImageData
-      }
-      alt={post.frontmatter.slug}
-    />
+import { CardPost, CardLink } from "../cards";
 
-    <div className={styles.body}>
-      <h2>{post.frontmatter.title}</h2>
-      <span className={styles.dateField}>
-        {post.frontmatter.date}
-      </span>
-      <Link
-        className="link ml-auto mt-4 mb-4"
-        to={post.frontmatter.slug}
+const WorkLink = ({ post }) => {
+  return (
+    <CardLink to={post.frontmatter.slug}>
+      <CardPost
+        imageData={post.frontmatter.featureImage}
+        imageAlt={post.frontmatter.slug}
+        key={post.id}
       >
-        Devamını Oku
-      </Link>
-    </div>
-  </div>
-);
+        <div className={styles.body}>
+          <h2>{post.frontmatter.title}</h2>
+          <span className={styles.dateField}>
+            {post.frontmatter.date}
+          </span>
+        </div>
+      </CardPost>
+    </CardLink>
+  );
+};
 
 export default WorkLink;
