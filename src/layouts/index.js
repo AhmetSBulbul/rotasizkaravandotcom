52d7cfbc53af999eb56b494e805d0232314bcea3
Ahmet Safa Bülbül <ahmetsafabulbul@gmail.com>
2021-10-06 21:39:10 +0300

    float call to action btn

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,6 +3,7 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { Helmet } from "react-helmet";
 import * as styles from "./index.module.css";
+import FloatContactButton from "../components/FloatContactButton";
 
 const Layout = ({ children }) => {
   return (
@@ -11,6 +12,7 @@ const Layout = ({ children }) => {
         <Helmet title="Rotasız Karavan" />
         <Header />
         {children}
+        <FloatContactButton />
         <Footer />
       </div>
     </div>
